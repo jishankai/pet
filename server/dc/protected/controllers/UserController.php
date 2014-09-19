df9f7bcb296eec95579773d0f0bfb7ab501d47ad
skji <jishankai@gmail.com>
2014-09-19 14:36:18 +0800

    MODIFY

--- server/dc/protected/controllers/UserController.php
+++ server/dc/protected/controllers/UserController.php
@@ -337,7 +337,7 @@ class UserController extends Controller
             $usr_id = $this->usr_id; 
         }
         if ($is_simple) {
-            $r = Yii::app()->db->createCommand('SELECT a.aid, a.tx, a.name, a.type, a.age FROM dc_circle c LEFT JOIN dc_animal a ON c.aid=a.aid WHERE c.usr_id=:usr_id')->bindValue(':usr_id', $usr_id)->queryAll();
+            $r = Yii::app()->db->createCommand('SELECT a.aid, a.tx, a.name, a.type, a.age, a.gender FROM dc_circle c LEFT JOIN dc_animal a ON c.aid=a.aid WHERE c.usr_id=:usr_id')->bindValue(':usr_id', $usr_id)->queryAll();
         } else {
             $r = Yii::app()->db->createCommand('SELECT a.aid, a.tx, a.name, a.master_id, a.d_rq, c.t_contri, c.rank, (SELECT COUNT(*) FROM dc_news n WHERE c.aid=n.aid) AS news_count, (SELECT COUNT(*) FROM dc_circle c1 WHERE c.aid=c1.aid) AS fans_count FROM dc_circle c LEFT JOIN dc_animal a ON c.aid=a.aid WHERE c.usr_id=:usr_id')->bindValue(':usr_id', $usr_id)->queryAll();
         }
