288b66b75736659de4525def2a0c05b8d91f8e99
skji <jishankai@gmail.com>
2014-10-29 20:17:02 +0800

    UPDATE

--- server/dc/protected/modules/admin/views/tool/index.php
+++ server/dc/protected/modules/admin/views/tool/index.php
@@ -5,7 +5,7 @@ $this->breadcrumbs=array(
 );
 
 $this->menu=array(
-	array('label'=>'切换用户', 'url'=>array('change')),
+	array('label'=>'切换用户', 'url'=>array('replace')),
 	array('label'=>'删除用户', 'url'=>array('clear')),
 );
 ?>
