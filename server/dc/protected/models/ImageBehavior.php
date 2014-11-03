7593fe297e82c81931d39c9f281f9cd4b1ac9daf
skji <jishankai@gmail.com>
2014-11-03 19:29:23 +0800

    UDPATE

--- server/dc/protected/models/ImageBehavior.php
+++ server/dc/protected/models/ImageBehavior.php
@@ -3,7 +3,7 @@
 class ImageBehavior extends CActiveRecordBehavior
 {
     public function showImage(){
-        return CHtml::image('http://pet4upload.oss-cn-beijing-internal.aliyuncs.com/'.$this->owner->url, $this->owner->url, array('max-width'=>'240px','max-height'=>'320px'));
+        return CHtml::image('http://pet4upload.oss-cn-beijing.aliyuncs.com/'.$this->owner->url, $this->owner->url, array('max-width'=>'240px','max-height'=>'320px'));
     }   
 
     public function getTopic(array $bar)
