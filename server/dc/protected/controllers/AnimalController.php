fa4f09a498e1ea74eb72c889d60609a5a24189a6
skji <jishankai@gmail.com>
2014-11-07 18:05:08 +0800

    UPDATE

--- server/dc/protected/controllers/AnimalController.php
+++ server/dc/protected/controllers/AnimalController.php
@@ -729,7 +729,8 @@ class AnimalController extends Controller
         $session = Yii::app()->session;
         $max_rq = Yii::app()->db->createCommand('SELECT MAX(t_rq) FROM dc_animal')->queryScalar();
         foreach ($r as $k=>$v) {
-            $r[$k]['images'] = Yii::app()->db->createCommand('SELECT img_id, url FROM dc_image WHERE aid=:aid ORDER BY update_time LIMIT 4')->bindValue(':aid', $v['aid'])->queryAll();
+            $aid = $v['aid'];
+            $r[$k]['images'] = Yii::app()->db->createCommand('SELECT img_id, url FROM dc_image WHERE aid=:aid ORDER BY update_time LIMIT 4')->bindValue(':aid', $aid)->queryAll();
             $r[$k]['percent'] = ceil($v['t_rq']*100/$max_rq);
             $r[$k]['shake_count'] = $session[$aid.'_shake_count'];
             $r[$k]['gift_count'] = $session[$aid.'_gift_count'];
