4f2d9888217cffe03f34a2fdf61fceeeae5a6dfa
skji <jishankai@gmail.com>
2014-11-14 11:19:57 +0800

    UPDATE

--- server/dc/protected/controllers/GameController.php
+++ server/dc/protected/controllers/GameController.php
@@ -13,7 +13,7 @@ class GameController extends Controller
     public function action2048($aid)
     {
         $this->layout = FALSE;
-        $this->render('dcz');
+        $this->render('dcz_in_app');
     }
 
     public function actionDcz($aid)
