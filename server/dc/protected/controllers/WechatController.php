12eb73a0c99a8d6f6c205d6bdd040ef9d7666b38
skji <jishankai@gmail.com>
2014-11-24 19:28:23 +0800

    UPDATE

--- server/dc/protected/controllers/WechatController.php
+++ server/dc/protected/controllers/WechatController.php
@@ -9,6 +9,33 @@ class WechatController extends Controller
         );
     }
 
+    public function actionGetMenuApi()
+    {
+        $appid="";//填写appid
+        $secret="";//填写secret
+
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={$appid}&secret={$secret}";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $a = curl_exec($ch);
+        $strjson=json_decode($a);
+        $token = $strjson->access_token;
+        
+        $url = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token={$token}"; //查询地址 
+        $ch = curl_init();//新建curl
+        curl_setopt($ch, CURLOPT_URL, $url);//url  
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $b = curl_exec($ch); //输出   
+        curl_close($ch); 
+
+        echo $b;
+    }
+
     public function actionUpdateMenuApi()
     {
         $appid="";//填写appid
