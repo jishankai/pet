5a3d39c3e24003ac7a8828d050a341a0d2d3b32e
skji <jishankai@gmail.com>
2014-12-04 18:17:43 +0800

    UPDATE

--- server/dc/protected/controllers/ImageController.php
+++ server/dc/protected/controllers/ImageController.php
@@ -294,9 +294,9 @@ class ImageController extends Controller
         )); 
     }
 
-    public function actionAsk4FoodApi()
+    public function actionAsk4FoodApi($page=0)
     {
-       $r = Yii::app()->db->createCommand('SELECT i.img_id, i.aid, i.cmt, i.food, i.create_time, a.name, a.tx, a.type, a.gender, u.usr_id, u.tx AS u_tx, u.name AS u_name  FROM dc_image i LEFT JOIN dc_animal a ON i.aid=a.aid LEFT JOIN dc_user u ON a.master_id=u.usr_id WHERE is_food=1 AND i.create_time>=:create_time')->bindValue(':create_time', time()-(60*60*24))->queryAll();
+       $r = Yii::app()->db->createCommand('SELECT i.img_id, i.aid, i.cmt, i.food, i.create_time, a.name, a.tx, a.type, a.gender, u.usr_id, u.tx AS u_tx, u.name AS u_name  FROM dc_image i LEFT JOIN dc_animal a ON i.aid=a.aid LEFT JOIN dc_user u ON a.master_id=u.usr_id WHERE is_food=1 AND i.create_time>=:create_time ORDER BY create_time DESC LIMIT :m,30')->bindValues(array(':create_time'=> time()-(60*60*24), ':m'=>$page*30))->queryAll();
 
        $this->echoJsonData(array($r));
     }
