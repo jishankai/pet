4903dd52cf5c5ad94c29a23daafd32ecd1ab0a81
skji <jishankai@gmail.com>
2014-12-09 14:51:40 +0800

    UPDATE

--- server/dc/protected/controllers/UserController.php
+++ server/dc/protected/controllers/UserController.php
@@ -7,7 +7,7 @@ class UserController extends Controller
         return array(
             'checkUpdate',
             'checkSig',
-            'getUserId - welcomeApi,getSIDApi,planetApi,loginApi,typeApi,bindApi,registerApi,othersApi,infoApi,petsApi,followingApi,topicApi,itemsApi,recommendApi,searchApi,reportApi',
+            'getUserId - welcomeApi,getSIDApi,planetApi,loginApi,typeApi,bindApi,registerApi,othersApi,infoApi,petsApi,followingApi,topicApi,itemsApi,recommendApi,searchApi,reportApi,loginBy3PartyApi,bindUserApi',
             /*
             array(
                 'COutputCache + welcomeApi',
