1fbb72019341040acc9574ae811ae595ddc45dae
skji <jishankai@gmail.com>
2014-12-11 18:02:14 +0800

    UPDATE

--- server/dc/protected/controllers/ImageController.php
+++ server/dc/protected/controllers/ImageController.php
@@ -133,6 +133,7 @@ class ImageController extends Controller
                         }
                     }
 
+                    /*
                     $animal = Animal::model()->findByPk($aid);
                     $circles = Circle::model()->findByAttributes(array('aid'=>$aid));
                     foreach ($circles as $circle) {
@@ -140,6 +141,7 @@ class ImageController extends Controller
                             Talk::model()->sendMsg(NPC_IMAGE_USRID, $circle->usr_id, "大萌星".$animal->name."发布了一张挣口粮的新萌照，快去支持吧～");
                         }
                     }
+                     */
                 }
                 $transaction->commit();
             } catch (Exception $e) {
