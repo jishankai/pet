c1c7e7218afe845214e93796e28d1040cd8aef6e
skji <jishankai@gmail.com>
2015-01-06 13:43:59 +0800

    UPDATE

--- server/dc/protected/views/social/activity_index.php
+++ server/dc/protected/views/social/activity_index.php
@@ -4,24 +4,24 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>暖冬活动进行中</title>
-	<link rel="stylesheet" type="text/css" href="act1_style.css">
+	<link rel="stylesheet" type="text/css" href="css/act1_style.css">
 
 </head>
 <body>
 <div class="act1_wrap comWidth">
 	<div class="act1_top">
-		<img alt="" src="images/act2.png">
+		<img alt="" src="css/images/act2.png">
 	</div>
 	<div class="act1_body">
 		<div class="ab_tit">代粮喵们</div>
 		<div class="act1_imgBox">
 			<ul class="act1_imgList clearfix">
-				<li><img alt="" src="images/a1.png"></li>
-				<li><img alt="" src="images/a2.png"></li>
-				<li><img alt="" src="images/a3.png"></li>
-				<li><img alt="" src="images/a4.png"></li>
-				<li><img alt="" src="images/a5.png"></li>
-				<li><img alt="" src="images/a6.png"></li>
+				<li><img alt="" src="css/images/a1.png"></li>
+				<li><img alt="" src="css/images/a2.png"></li>
+				<li><img alt="" src="css/images/a3.png"></li>
+				<li><img alt="" src="css/images/a4.png"></li>
+				<li><img alt="" src="css/images/a5.png"></li>
+				<li><img alt="" src="css/images/a6.png"></li>
 			</ul>
 		</div>
 	</div>
@@ -30,35 +30,35 @@
 		<div class="head_imgBox">
 			<ul class="head_imgList clearfix">
 				<li style="position: relative">
-					<img alt="" src="images/base_head1.JPG">
-					<img src="images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+					<img alt="" src="css/images/base_head1.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
 				</li>
 				<li style="position: relative">
-					<img alt="" src="images/base_head2.JPG">
-					<img src="images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+					<img alt="" src="css/images/base_head2.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
 				</li>
 				<li style="position: relative">
-					<img alt="" src="images/base_head3.JPG">
-					<img src="images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+					<img alt="" src="css/images/base_head3.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
 				</li>
 				<li style="position: relative">
-					<img alt="" src="images/base_head4.JPG">
-					<img src="images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+					<img alt="" src="css/images/base_head4.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
 				</li>
 				<li style="position: relative">
-					<img alt="" src="images/base_head5.JPG">
-					<img src="images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+					<img alt="" src="css/images/base_head5.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
 				</li>
 				<li style="position: relative">
-					<img alt="" src="images/base_head6.JPG">
-					<img src="images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+					<img alt="" src="css/images/base_head6.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
 				</li>
 				<li style="position: relative">
-					<img alt="" src="images/base_head7.JPG">
-					<img src="images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+					<img alt="" src="css/images/base_head7.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
 				</li>
 				<li>
-					<img alt="" src="images/b_right.png" class="head_right">
+					<img alt="" src="css/images/b_right.png" class="head_right">
 				</li>
 			</ul>
 		</div>
@@ -69,7 +69,7 @@
 		<div class="load clearfix">				
 			<div class="load_left">
 				<div class="logo_icon">
-					<img src="images/logo_icon.jpg"/>
+					<img src="css/images/logo_icon.jpg"/>
 				</div>
 				<div class="load_info">
 					<h3>宠物星球</h3>
@@ -77,7 +77,7 @@
 				</div>
 			</div>
 			<div class="load_right">
-				<img src="images/load.jpg"/>
+				<img src="css/images/load.jpg"/>
 			</div>
 		</div>
 	</div>
