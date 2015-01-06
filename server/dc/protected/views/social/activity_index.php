66186bb4ac47ad454604d523e1c58175e51f3c03
skji <jishankai@gmail.com>
2015-01-06 10:25:58 +0800

    UPDATE

--- server/dc/protected/views/social/activity_index.php
+++ server/dc/protected/views/social/activity_index.php
@@ -1,128 +1,66 @@
 <!DOCTYPE>
 <html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		<title>wram-winter-activities2</title>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>暖冬活动进行中</title>
+	<link rel="stylesheet" type="text/css" href="css/act1_style.css">
 
-		<link rel="stylesheet" type="text/css" href="css/w_act2.css">
-
-		<script src="http://libs.baidu.com/jquery/1.9.0/jquery.js" type="text/javascript"></script>
-	</head>
-
+</head>
 <body>
-<div class="wrapper">
-	<div class="bhead">
-		<img class="b_act" src="css/images/act2.png">
+<div class="act1_wrap comWidth">
+	<div class="act1_top">
+		<img alt="" src="css/images/act2.png">
 	</div>
-	<div class="b_body">
-		<h3>代粮喵们</h3>
-		<div class="bimg_box">
-			<ul class="bimg">
-				<li class="bcat1">
-					<div class="bcirle">1</div>
-					<div class="cat_in">
-						<h3>瓜皮</h3>
-						<p>北京香山</p>
-					</div>
-				</li>
-				<li class="bcat2">
-					<div class="bcirle">2</div>
-					<div class="cat_in">
-						<h3>十四阙</h3>
-						<p>北京香山</p>
-					</div>
-				</li>
-				<li class="bcat3">
-					<div class="bcirle">3</div>
-					<div class="cat_in">
-						<h3>税后五百</h3>
-						<p>北京香山</p>
-					</div>
-				</li>
-				<li class="bcat4">
-					<div class="bcirle">4</div>
-					<div class="cat_in">
-						<h3>财猫梁大</h3>
-						<p>北京香山</p>
-					</div>
-				</li>
-				<li class="bcat5">
-					<div class="bcirle">5</div>
-					<div class="cat_in">
-						<h3>端午</h3>
-						<p>北京香山</p>
-					</div>
-				</li>
-				<li class="bcat6">
-					<div class="bcirle">6</div>
-					<div class="cat_in">
-						<h3>海顿</h3>
-						<p>北京香山</p>
-					</div>
-				</li>	
-				</ul>
+	<div class="act1_body">
+		<div class="ab_tit">代粮喵们</div>
+		<div class="act1_imgBox">
+			<ul class="act1_imgList clearfix">
+				<li><img alt="" src="css/images/2_1.png"></li>
+				<li><img alt="" src="css/images/2_2.png"></li>
+				<li><img alt="" src="css/images/2_3.png"></li>
+				<li><img alt="" src="css/images/2_4.png"></li>
+				<li><img alt="" src="css/images/2_5.png"></li>
+				<li><img alt="" src="css/images/2_6.png"></li>
+			</ul>
 		</div>
 	</div>
-	<div class="b_bottom">
-		<h3 class="help_tit">TA们正在参与伸出援手</h3>
-		<ul class="help_head">
-			<li style="position: relative">
-				<img src="css/images/base_head1.JPG ">
-				<img src="css/images/photo-m-t.png" style="position: absolute;
-	left:0;top:0px;z-index:1；">
-			</li>
-			<li style="position: relative">
-				<img src="css/images/base_head2.JPG">
-				<img src="css/images/photo-m-t.png" style="position: absolute;
-	left:0;top:0px;z-index:1；">
-			</li>
-			<li style="position: relative">
-				<img src="css/images/base_head3.JPG">
-				<img src="css/images/photo-m-t.png" style="position: absolute;
-	left:0;top:0px;z-index:1；">
-			</li>
-			<li style="position: relative">
-				<img src="css/images/base_head4.JPG">
-				<img src="css/images/photo-m-t.png" style="position: absolute;
-	left:0;top:0px;z-index:1；">
-			</li>
-			<li style="position: relative">
-				<img src="css/images/base_head5.JPG">
-				<img src="css/images/photo-m-t.png" style="position: absolute;
-	left:0;top:0px;z-index:1；">
-			</li>
-			<li style="position: relative">
-				<img src="css/images/base_head6.JPG">
-				<img src="css/images/photo-m-t.png" style="position: absolute;
-	left:0;top:0px;z-index:1；">
-			</li>
-			<li style="position: relative">
-				<img src="css/images/base_head7.JPG">
-				<img src="css/images/photo-m-t.png" style="position: absolute;
-	left:0;top:0px;z-index:1；">
-			</li>
-			<li><a href="#" class="b_more"><img src="css/images/b_right.png"></a></li>
-		</ul>
-		<h3 class="p_num">参与人数 <span>123445</span></h3>
-	</div>
-	<div class="float">
-		<div class="load">				
-			<div class="load_left">
-				<div class="logo_icon">
-					<img src="css/images/logo_icon.jpg"/>
-				</div>
-				<div class="load_info">
-					<h3>宠物星球</h3>
-					<p>我为自己代粮</p>
-				</div>
-			</div>
-			<div class="load_right">
-				<img src="css/images/load.jpg"/>
-			</div>
+	<div class="act1_bottom">
+		<div class="am_tit">TA们正在参与伸出援手</div>
+		<div class="head_imgBox">
+			<ul class="head_imgList clearfix">
+				<li style="position: relative">
+					<img alt="" src="css/images/base_head1.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+				</li>
+				<li style="position: relative">
+					<img alt="" src="css/images/base_head2.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+				</li>
+				<li style="position: relative">
+					<img alt="" src="css/images/base_head3.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+				</li>
+				<li style="position: relative">
+					<img alt="" src="css/images/base_head4.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+				</li>
+				<li style="position: relative">
+					<img alt="" src="css/images/base_head5.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+				</li>
+				<li style="position: relative">
+					<img alt="" src="css/images/base_head6.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+				</li>
+				<li style="position: relative">
+					<img alt="" src="css/images/base_head7.JPG">
+					<img src="css/images/photo-m-t.png" style="position: absolute;left:0;top:0px;z-index:1；">
+				</li>
+			</ul>
 		</div>
+		<div class="person_num">参与人数<span>123445</span></div>
 	</div>
 </div>
 </body>
-	<script type="text/javascript" src="js/alink.js"></script>
 </html>
