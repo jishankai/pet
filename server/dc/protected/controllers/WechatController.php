3f853f174072f542fc9ec6c7d077334ec9c9362a
skji <jishankai@gmail.com>
2015-01-08 12:22:58 +0800

    UPDATE

--- server/dc/protected/controllers/WechatController.php
+++ server/dc/protected/controllers/WechatController.php
@@ -380,6 +380,7 @@ class WechatController extends Controller
                 'u_name'=>$u['nickname'],
                 'u_gender'=>$u['sex'],
                 'u_city'=>1001,
+                'code'=>'',
                 'wechat'=>$u['openid'],
                 'SID'=>$j->data->SID,
             );
@@ -392,7 +393,7 @@ class WechatController extends Controller
         }
         $this->layout = FALSE;
         $r = Yii::app()->db->createCommand('SELECT i.img_id, i.url, i.aid, i.cmt, i.food, i.create_time, a.name, a.tx, a.type, a.gender, u.usr_id, u.tx AS u_tx, u.name AS u_name  FROM dc_image i LEFT JOIN dc_animal a ON i.aid=a.aid LEFT JOIN dc_user u ON a.master_id=u.usr_id WHERE i.img_id=:img_id')->bindValue(':img_id', $state)->queryRow();
-        $this->render('food', array('r'=>$r, 'sid'=>$j->data->SID));
+        $this->render('/social/food', array('r'=>$r, 'sid'=>$j->data->SID));
     }
 
 }
--- server/dc/protected/controllers/WeiboController.php
+++ server/dc/protected/controllers/WeiboController.php
@@ -36,6 +36,7 @@ class WeiboController extends Controller
                 	'gender'=>$r['gender'],
                 	'age'=>$r['age'],
                 	'type'=>$r['type'],
+                	'code'=>'',
                 	'u_name'=>$u['name'],
                 	'u_gender'=>$u['sex']=='m'?1:2,
                 	'u_city'=>1001,
@@ -51,7 +52,7 @@ class WeiboController extends Controller
         	}
         	$this->layout = FALSE;
         	$r = Yii::app()->db->createCommand('SELECT i.img_id, i.url, i.aid, i.cmt, i.food, i.create_time, a.name, a.tx, a.type, a.gender, u.usr_id, u.tx AS u_tx, u.name AS u_name  FROM dc_image i LEFT JOIN dc_animal a ON i.aid=a.aid LEFT JOIN dc_user u ON a.master_id=u.usr_id WHERE i.img_id=:img_id')->bindValue(':img_id', $state)->queryRow();
-        	$this->render('food', array('r'=>$r, 'sid'=>$j->data->SID));
+        	$this->render('/social/food', array('r'=>$r, 'sid'=>$j->data->SID));
 		} else {
 		    echo '认证失败';
 		}
