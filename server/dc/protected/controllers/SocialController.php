44d80c8cb3d5e188d5d7ebe23885e2420a9c09ea
skji <jishankai@gmail.com>
2015-01-10 15:02:14 +0800

    UPDATE

--- server/dc/protected/controllers/SocialController.php
+++ server/dc/protected/controllers/SocialController.php
@@ -8,11 +8,11 @@ class SocialController extends Controller
         );
     }
 
-    public function actionFoodShareApi($img_id, $to='', $SID='')
+    public function actionFoodShareApi($img_id, $alert_flag=0, $to='', $SID='')
     {
         $r = Yii::app()->db->createCommand('SELECT i.img_id, i.url, i.aid, i.cmt, i.food, i.create_time, a.name, a.tx, a.type, a.gender, u.usr_id, u.tx AS u_tx, u.name AS u_name  FROM dc_image i LEFT JOIN dc_animal a ON i.aid=a.aid LEFT JOIN dc_user u ON a.master_id=u.usr_id WHERE i.img_id=:img_id')->bindValue(':img_id', $img_id)->queryRow();
 
-        $this->renderPartial('food', array('r'=>$r, 'img_id'=>$img_id, 'aid'=>$r['aid'], 'to'=>$to, 'sid'=>$SID));
+        $this->renderPartial('food', array('r'=>$r, 'img_id'=>$img_id, 'alert_flag'=>$alert_flag, 'aid'=>$r['aid'], 'to'=>$to, 'sid'=>$SID));
     }
 
     public function actionNewYearEvent($SID='')
@@ -22,9 +22,11 @@ class SocialController extends Controller
         $this->renderPartial('activity_index', array('users'=>$users, 'sid'=>$SID));
     }
 
-    public function actionActivityview($aid, $SID='')
+    public function actionActivityview($aid, $alert_flag=0, $SID='')
     {
-        $this->renderPartial('activity_view_'.$aid, array('aid'=>$aid, 'sid'=>$SID));
+        $food = Yii::app()->db->createCommand('SELECT food FROM dc_animal WHERE aid=:aid')->bindValue(':aid', $aid)->queryScalar();
+        
+        $this->renderPartial('activity_view_'.$aid, array('aid'=>$aid, 'alert_flag'=$alert_flag, 'food'=>$food, 'sid'=>$SID));
     }
 
 }
