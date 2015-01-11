37bcd3b33bfdfdc687f40b39c8a660e420541272
skji <jishankai@gmail.com>
2015-01-11 15:04:37 +0800

    UPDATE

--- server/dc/protected/views/social/activity_index.php
+++ server/dc/protected/views/social/activity_index.php
@@ -234,6 +234,14 @@ $signPackage = $jssdk->GetSignPackage();
   });
   wx.ready(function () {
     // 在这里调用 API
+    var shareData = {
+    	title: '微信JS-SDK Demo',
+    	desc: '微信JS-SDK,帮助第三方为用户提供更优质的移动web服务',
+    	link: 'http://demo.open.weixin.qq.com/jssdk/',
+    	imgUrl: 'http://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRt8Qia4lv7k3M9J1SKqKCImxJCt7j9rHYicKDI45jRPBxdzdyREWnk0ia0N5TMnMfth7SdxtzMvVgXg/0'
+    };
+    wx.onMenuShareAppMessage(shareData);
+    wx.onMenuShareTimeline(shareData);
   });
   wx.onMenuShareAppMessage({
       title: '互联网之子',
