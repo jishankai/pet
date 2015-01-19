759ce234457091090741db0020d36adc83ab007a
skji <jishankai@gmail.com>
2015-01-19 23:33:28 +0800

    UPDATE

--- server/dc/protected/views/social/activity_view_652.php
+++ server/dc/protected/views/social/activity_view_652.php
@@ -54,7 +54,7 @@ $signPackage = $jssdk->GetSignPackage();
 			<div class="target_Box clearfix">
 				<!-- <div class="target">目标<span class="target_num" id="target_num">342000</span>还剩 <span class="target_num" id="target_num"><?php echo 342000-$food ?></span> 份口粮</div>
 				<div class="d_time" id="d_time"></div> -->
-			     <div class="target">目标<span class="target_num" id="target_num">162000</span>达成！</div>
+			     <div class="target">目标<span class="target_num" id="target_num">342000</span>达成！</div>
                 <div class="d_time">达成时间：2015年1月19日23时06分</div>
             </div>
 			<div class="hr_10"></div>
