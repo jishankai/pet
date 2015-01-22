1172806a086fb849d6e1856f9caffb435a441f8c
skji <jishankai@gmail.com>
2015-01-22 17:43:49 +0800

    UPDATE

---
+++ server/dc/protected/views/site/faq.php
@@ -0,0 +1,310 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+		<title>help</title>
+		<link rel="stylesheet" type="text/css" href="css/help_style.css">
+	</head>
+<body>
+<div class="wraper" id="wraper_index">
+	<div class="help_tit">
+		<h3 class="tit_noicon">常见问题</h3>
+	</div>
+	<div class="help_body">
+		<ul class="help_list" id="help_list">
+			<li><a href="#">萌星与捧TA</a></li>
+			<li><a href="#">挣口粮与兑换</a></li>
+			<li><a href="#">贡献度</a></li>
+			<li><a href="#">萌星人气值</a></li>
+			<li><a href="#">基本动作</a></li>
+			<li><a href="#">金币</a></li>
+			<li><a href="#">献爱心</a></li>
+		</ul>
+	</div>
+</div>
+
+<!-- 萌星与捧TA -->
+<div class="wraper" id="help0">
+	<a href="http://192.168.10.151:8001/index.php?r=social/newYearEvent">
+		<div class="help_tit" id="help_tit">
+
+			<h3 class="tit">常见问题</h3>
+		</div>
+	</a>
+	<div class="Details_body">
+		<ul class="details_list">
+			<li>
+				<div class="list_tit">萌星是什么？</div>
+				<div class="list_con">
+				“萌星”是指宠物星球上的萌宠，纯真可爱的萌宠们，就像是宇宙中耀眼闪烁的明星，让无数地球人争相成为TA们的粉丝。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">捧TA是什么？</div>
+				<div class="list_con">地球人遇见心仪的萌宠，可以选择捧TA，成为萌星的粉丝，通过点赞、送礼物等活动帮TA增加人气。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何捧TA？</div>
+				<div class="list_con">
+					点击萌星资料右上角 “+”，在弹窗中选择“捧TA”，每个地球人可以免费捧10个萌星，超过10个要花费金币哦~
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何创建萌星？</div>
+				<div class="list_con">
+					在首页“我的萌星”页面，点击右上角的“+”创建新萌星。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何不捧了？</div>
+				<div class="list_con">
+					在首页“个人中心”点击头像进入“我的档案”，点击萌星选择“不捧了”。需要注意的是，地球人不能不捧自己创建的萌星和当前仅有的萌星。
+				</div>
+			</li>
+			
+		</ul>
+	</div>
+</div>
+
+<!-- 挣口粮与兑换 -->
+<div class="wraper" id="help1">
+<a href="http://192.168.10.151:8001/index.php?r=social/newYearEvent">
+	<div class="help_tit">
+
+		<h3 class="tit">常见问题</h3>
+	</div>
+</a>
+	<div class="Details_body">
+		<ul class="details_list">
+			<li>
+				<div class="list_tit">为什么要挣口粮？</div>
+				<div class="list_con">
+				宠物主人（经纪人）可以为您创建的每只宠物精心挑选并发布一张“挣口粮”的照片。喜欢它的用户会给萌星“打赏”口粮，当得到的口粮数达到一定数量，就可以兑换真实宠物粮啦~快来让你的萌星“为自己代粮”吧~
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何挣口粮？</div>
+				<div class="list_con">
+				宠物主人（经纪人）可以在首页“我的萌星”页面，点击“挣口粮”来为自己创建的萌星发布“挣口粮”照片（每24小时限发布一张“挣口粮”照片，且照片的有效期为发布后的24小时以内），发布成功后还可以分享至微博、微信，让小伙伴们一起为萌星送粮吧~粉丝也可以使用“挣口粮”，帮TA分享，为TA助力！
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何打赏？</div>
+				<div class="list_con">
+					首页“挣口粮”页面能看到所有正在“挣口粮”的照片，在这里就可以为您喜欢的萌星打赏口粮啦~不仅在应用内，分享至社交应用的网页版也可以进行打赏！需要注意的是，每位用户每日都会有免费的口粮可以随意打赏，打赏更多就需要花费金币了哟~
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何兑换？</div>
+				<div class="list_con">
+					收到口粮会积累至相应的萌星，当萌星得到的口粮数足够多时就可以在“兑换”页面换取实物啦~兑换成功后，只需要您填写好收货地址，工作人员就会尽快为您备货，快递上门哦~
+				</div>
+			</li>
+			
+		</ul>
+	</div>
+</div>
+
+<!-- 贡献度 -->
+
+<div class="wraper" id="help2">
+	<a href="http://192.168.10.151:8001/index.php?r=social/newYearEvent">
+		<div class="help_tit">
+
+			<h3 class="tit">常见问题</h3>
+		</div>
+	</a>
+	<div class="Details_body">
+		<ul class="details_list">
+			<li>
+				<div class="list_tit">贡献度是什么？</div>
+				<div class="list_con">
+				贡献度体现了地球人对萌星做出的贡献，给萌星送礼物会增加贡献度。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">什么是贡献排行榜？</div>
+				<div class="list_con">
+				是每个萌星的粉丝做出贡献的排行榜，分为日贡献榜、周贡献榜、月贡献榜和总贡献榜。结榜时排名前列的小伙伴可得到奖励，每个榜单在榜单周期结束当天的24:00结算发放奖励。（暂未开放）
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">什么是萌星的粉丝等级？</div>
+				<div class="list_con">
+					粉丝等级分为8级，分别是“凉粉、淡粉、狠粉、灰常粉、超级粉、铁杆粉、脑残粉、骨灰粉”，粉丝等级出现在萌星主页的各个角落，升级时还会获得奖励呦~
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">什么是经纪人？</div>
+				<div class="list_con">
+					萌星主人的等级会始终记为“经纪人”，象征独一无二的所属关系。带领你的萌宠闪闪发光吧！
+				</div>
+			</li>
+			
+		</ul>
+	</div>
+</div>
+
+<!-- 萌星人气值 -->
+
+<div class="wraper" id="help3">
+	<a href="http://192.168.10.151:8001/index.php?r=social/newYearEvent">
+		<div class="help_tit">
+
+			<h3 class="tit">常见问题</h3>
+		</div>
+	</a>
+	<div class="Details_body">
+		<ul class="details_list">
+			<li>
+				<div class="list_tit">萌星人气值是什么？</div>
+				<div class="list_con">
+					人气值是体现萌星在宠物星球受欢迎程度的数值。人气排名高的萌星是名副其实的明星，会得到欢迎页推荐的明星待遇，还有机会参加宠物星球举办的粉丝活动呢~。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何增加人气值？</div>
+				<div class="list_con">
+					萌星收到礼物、被赞、被评论、被关注都会增加人气值。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">人气值为什么会降低？</div>
+				<div class="list_con">
+					萌星收到捣乱礼物和被捣捣乱都会降低人气值。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">什么是星球人气排行榜？</div>
+				<div class="list_con">
+					星球人气排行榜是萌星的人气排行榜，分成日人气榜、周人气榜、月人气榜和总人气榜。结榜时排名前列萌星的粉丝会得到相应奖励，每个榜单会在榜单周期结束当天的24:00结算并发放奖励。（暂未开放）
+				</div>
+			</li>
+			
+		</ul>
+	</div>
+</div>
+<!-- 基本动作 -->
+<div class="wraper" id="help4">
+	<a href="http://192.168.10.151:8001/index.php?r=social/newYearEvent">
+		<div class="help_tit">
+
+			<h3 class="tit">常见问题</h3>
+		</div>
+	</a>
+	<div class="Details_body">
+		<ul class="details_list">
+			<li>
+				<div class="list_tit">什么是摇一摇？</div>
+				<div class="list_con">
+					用户每日可以通过摇动手机摇出礼物，送给相应的萌星。摇一摇送出的礼物同样可以增加萌星的人气值和用户的贡献度。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">什么是萌印象？</div>
+				<div class="list_con">
+					点击“萌印象”，会出现一张相应萌星的照片，完成小游戏就有机会获得金币奖励哦~
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">什么是游乐园？</div>
+				<div class="list_con">
+					游乐园是我们为大家准备的精彩小游戏，完成游戏，会根据成绩得到相应的金币奖励~（暂未开放）
+				</div>
+			</li>		
+		</ul>
+	</div>
+</div>
+
+<!-- 金币 -->
+<div class="wraper" id="help5">
+	<a href="http://192.168.10.151:8001/index.php?r=social/newYearEvent">
+		<div class="help_tit">
+
+			<h3 class="tit">常见问题</h3>
+		</div>
+	</a>
+	<div class="Details_body">
+		<ul class="details_list">
+			<li>
+				<div class="list_tit">如何获得金币？</div>
+				<div class="list_con">
+					有两种方式可以获得金币：
+					<p>登录应用、萌印象、发表评论、官衔提升和参与宠物星球丰富的活动均有机会获得金币奖励。</p>
+					<p>充值来拥有更多金币！（暂未开放）</p>
+
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">金币可以用来做什么？</div>
+				<div class="list_con">
+					金币可以用于打赏您喜欢的萌星；也可以购买礼物送给喜欢的萌星来为TA增加人气。
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+
+<!-- 献爱心 -->
+<div class="wraper" id="help6">
+	<a href="http://192.168.10.151:8001/index.php?r=social/newYearEvent">
+		<div class="help_tit">
+
+			<h3 class="tit">常见问题</h3>
+		</div>
+	</a>
+	<div class="Details_body">
+		<ul class="details_list">
+			<li>
+				<div class="list_tit">什么是虚拟礼物和现实礼物？</div>
+				<div class="list_con">
+					宠物星球的礼物分为虚拟礼物和现实礼物，虚拟礼物仅存在于宠物星球的世界中，现实礼物是派送到主人家里的真实礼品，收到虚拟礼物和现实礼物都可以增加萌星的人气值。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">为什么需要送礼物？</div>
+				<div class="list_con">
+					给萌星送礼物可以增加萌星的人气值和地球人的贡献度，粉丝们可以共同努力将萌星打造成全宇宙最闪亮萌星。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何献爱心？</div>
+				<div class="list_con">
+					您可以在送礼物弹窗中点击礼物直接送出，也可以先在商城购买完礼物之后在萌星主页送出礼物。
+				</div>
+			</li>
+			<li>
+				<div class="list_tit">如何送出现实礼物？</div>
+				<div class="list_con">
+					首先在礼物商城或送礼物弹框中购买礼物并选择赠送，之后萌星的经纪人会收到填写收货地址的私信通知，宠物星球的工作人员会在得到收货地址后的5个工作日内发出快递并私信通知您快递单号，经纪人收到礼物后会晒出礼物来感谢有爱的地球人的。（暂未开放）
+				</div>
+			</li>
+		</ul>
+	</div>
+</div>
+
+</body>
+<script type="text/javascript">
+window.onload = function(){
+
+	var hTab = document.getElementById("help_list");
+	var hLis = hTab.getElementsByTagName("li");
+	for(var i=0; i<7; i++){
+
+		hLis[i].index= i;
+
+		hLis[i].onclick = function(){
+
+			document.getElementById("wraper_index").style.display = "none";		
+			var a = "help" + this.index;
+			document.getElementById(a).style.display = "block";
+
+		}
+	}
+	
+}
+
+</script>
+</html>
