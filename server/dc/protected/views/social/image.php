c3483eb6eb97c45bc4d20dadcd12a540d20246e4
skji <jishankai@gmail.com>
2015-01-26 13:25:46 +0800

    UPDATE

---
+++ server/dc/protected/views/social/image.php
@@ -0,0 +1,173 @@
+<!DOCTYPE>
+<html>
+<head>
+	<meta name="viewport" content="width = device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>宠物主页</title>
+	<link rel="stylesheet" type="text/css" href="css/pet_index.css">
+	<script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+</head>
+<body>
+	<div class="wraper comWidth">
+		<div class="pi_top">
+			<div class="hr_125"></div>
+			<div class="header clearfix">
+				<img src="css/images/bg_headimg.png" class="pi_head_wrap">
+				<img src="css/images/bt_follow.png" id="bt_fllow">
+				<img src="css/images/a4.jpg" id="pi_head">
+				<img src="css/images/base_head1.jpg" id="owner_head">
+				<div class="owner">经纪人</div>
+			</div>
+		 
+			<div class="pi_name"><span id="name">毛毛和花花 </span><img src="css/images/female1.png" id="pi_sex"></div>
+
+			<div class="pet_info clearfix">
+				<div id="breed">中华田园猫</div>
+				<div id="vertical">|</div>
+				<div id="age">3岁1个月</div>
+			</div>
+			<h3 id="signature">我们的目标是，没有蛀牙！</h3>
+
+			<ul class="tab clearfix">
+				<li>
+					<h3 id="dynamic_num">115</h3>
+					<p id="dynamic">动态</p>
+				</li>
+				<li class="middle">
+					<h3 id="fans_num">115</h3>
+					<p id="fans">粉丝</p>
+				</li>
+				<li>
+					<h3 id="photo_num">115</h3>
+					<p id="photo">照片</p>
+				</li>
+			</ul>
+		</div>
+
+		<div class="pi_body">
+			<ul class="imgBox clearfix" id="imgBox">
+				<!-- <li><img src="css/images/pet1.jpg" alt="1"></li>
+				<li><img src="css/images/pet2.jpg" alt="2"></li>
+				<li><img src="css/images/pet3.jpg" alt="3"></li>
+				<li><img src="css/images/pet4.jpg" alt="4"></li>
+				<li><img src="css/images/pet5.jpg" alt="5"></li>
+				<li><img src="css/images/pet6.jpg" alt="6"></li>
+				<li><img src="css/images/pet7.jpg" alt="7"></li>
+				<li><img src="css/images/pet8.jpg" alt="8"></li>
+				<li><img src="css/images/pet9.jpg" alt="9"></li> -->
+			</ul>
+		</div>
+		<div class="hr_125"></div>
+		
+		<div class="float">
+		<div class="load_shadow"><img src="css/images/load_shadow.png"></div>
+			<div class="load">
+
+				<div class="load_left">
+					<div class="logo_icon">
+						<img src="css/images/logo_icon.jpg"/>
+					</div>
+					<div class="load_info">
+						<h3>宠物星球</h3>
+						<p>我为自己代粮</p>
+					</div>
+				</div>
+				<div class="load_right">
+					<img src="css/images/load.jpg"/>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</body>
+<script type="text/javascript">
+
+$(window).on('load',function(){
+	var h=$('#imgBox li').width();
+	console.log(h);
+	$('#imgBox li').height(h);
+	var dataInt={'data':[{'src':'pet1.jpg'},{'src':'pet2.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet4.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet3.jpg'},{'src':'pet0.jpg'},{'src':'pet8.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'}
+	,{'src':'pet3.jpg'},{'src':'pet4.jpg'},{'src':'pet4.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet6.jpg'}]};
+	var a = $('#imgBox img');
+	
+	for(var i=0;i<dataInt.data.length;i++){
+
+		var oLi=$('<li>').appendTo($('#imgBox'));
+		var oImg=$('<img>').attr('src','css/images/'+$(dataInt.data[i]).attr('src')).appendTo($(oLi));
+		var h=$('#imgBox li').width();
+		$('#imgBox li').height(h);
+		
+	}
+	$(window).on('scroll',function(){
+		if(checkScrollSlide()){
+			
+				loadImg();
+		
+			}			
+	})
+})
+
+function checkScrollSlide(){
+	var $lastBox=$('#imgBox>li').last();
+	var lastBoxLis = $lastBox.get(0).offsetTop + Math.floor($lastBox.height()/2);
+	var scrollTop=$(window).scrollTop();
+	var documentH=$(window).height();
+	return (lastBoxLis < scrollTop + documentH )?true:false;
+}
+function loadImg(){
+
+	var dataInt={'data':[{'src':'pet1.jpg'},{'src':'pet2.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet4.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet3.jpg'},{'src':'pet0.jpg'},{'src':'pet8.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet3.jpg'}
+	,{'src':'pet3.jpg'},{'src':'pet4.jpg'},{'src':'pet4.jpg'},{'src':'pet3.jpg'},
+	{'src':'pet3.jpg'},{'src':'pet3.jpg'},{'src':'pet6.jpg'}]};
+
+	for(var i=0;i<dataInt.data.length;i++){
+
+				var oLi=$('<li>').appendTo($('#imgBox'));
+				var oImg=$('<img>').attr('src','css/images/'+$(dataInt.data[i]).attr('src')).appendTo($(oLi));
+				
+				var h=$('#imgBox li').width();
+				$('#imgBox li').height(h);
+
+			}
+
+
+
+	 // ajax请求数据  
+    /*jQuery.ajax({  
+        type:"POST",  
+        url: "/show/getPins/",  
+        data:data,  
+        dataType: "json",  
+        beforeSend: function(XMLHttpRequest){  
+          $("#loading").css('display','');  
+        },  
+        success:function(response) {  
+          if(response.data){  
+            for(var i=0, length = response.data.length; i<length; i++){  
+                var html = response.data[i];  
+                var test = $(html);  
+                target.append(test);  
+                var img = test.find('img');  
+                X.util.flowPin(img[0],188);  
+            }  
+              
+                target.attr('index',parseInt(current_page)+1);  
+               
+                $("#loading").css('display','none');  
+          }  
+    },  
+    error:function(){  
+          alert("加载失败");  
+    }  
+    });  */
+
+}
+</script>
+</html>
