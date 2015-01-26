9ef9de8e97d30918917fef07f0b935196c2378a9
skji <jishankai@gmail.com>
2015-01-26 17:23:37 +0800

    UPDATE

--- server/dc/js/link.js
+++ server/dc/js/link.js
@@ -1,3 +1,20 @@
+/*选项卡*/
+var oTab = document.getElementById("tabs");
+var oUl = oTab.getElementsByTagName("ul")[0];
+var oLis = oUl.getElementsByTagName("li");
+var oDivs= oTab.getElementsByTagName("div");
+
+for(var i= 0,len = oLis.length;i<len;i++){
+   oLis[i].index = i;
+   oLis[i].onclick = function() {
+       for(var n= 0;n<len;n++){
+           oLis[n].className = "";
+           oDivs[n].className = "hide";
+       }
+       this.className = "on";
+       oDivs[this.index].className = "";
+   }
+}
 function checkScrollSlide(){
     var tabTop=$('#tabs').height();
     var scrollTop=$(window).scrollTop();
