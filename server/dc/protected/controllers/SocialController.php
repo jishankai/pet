d5d5024fc70610cd0b8083ce021708e2a83e4e11
skji <jishankai@gmail.com>
2015-01-27 16:32:36 +0800

    UPDATE

--- server/dc/protected/controllers/SocialController.php
+++ server/dc/protected/controllers/SocialController.php
@@ -10,7 +10,7 @@ class SocialController extends Controller
 
     public function actionFoodShareApi($img_id, $alert_flag=0, $to='', $SID='')
     {
-        $r = Yii::app()->db->createCommand('SELECT i.img_id, i.url, i.aid, i.cmt, i.food, i.create_time, a.name, a.tx, a.type, a.gender, u.usr_id, u.tx AS u_tx, u.name AS u_name  FROM dc_image i LEFT JOIN dc_animal a ON i.aid=a.aid LEFT JOIN dc_user u ON a.master_id=u.usr_id WHERE i.img_id=:img_id')->bindValue(':img_id', $img_id)->queryRow();
+        $r = Yii::app()->db->createCommand('SELECT i.img_id, i.url, i.aid, i.cmt, i.food, i.is_food, i.create_time, a.name, a.tx, a.type, a.gender, u.usr_id, u.tx AS u_tx, u.name AS u_name  FROM dc_image i LEFT JOIN dc_animal a ON i.aid=a.aid LEFT JOIN dc_user u ON a.master_id=u.usr_id WHERE i.img_id=:img_id')->bindValue(':img_id', $img_id)->queryRow();
         
         $pet_type = Util::loadConfig('pet_type');
         $n = floor($r['type']/100);
@@ -31,7 +31,11 @@ class SocialController extends Controller
             }
         }        
 
-        $this->renderPartial('food', array('r'=>$r, 'a_type'=>$a_type, 'img_id'=>$img_id, 'alert_flag'=>$alert_flag, 'aid'=>$r['aid'], 'to'=>$to, 'sid'=>$SID));
+        if ($r['is_food']) {
+            $this->renderPartial('food', array('r'=>$r, 'a_type'=>$a_type, 'img_id'=>$img_id, 'alert_flag'=>$alert_flag, 'aid'=>$r['aid'], 'to'=>$to, 'sid'=>$SID));
+        } else {
+            $this->renderPartial('image', array('r'=>$r, 'a_type'=>$a_type, 'img_id'=>$img_id, 'alert_flag'=>$alert_flag, 'aid'=>$r['aid'], 'to'=>$to, 'sid'=>$SID));
+        }
     }
 
     public function actionNewYearEvent($SID='')
