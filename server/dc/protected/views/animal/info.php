f6b18f8ce69e0c9793ede305857a1e65d94b8720
skji <jishankai@gmail.com>
2015-02-02 20:43:05 +0800

    UPDATE

--- server/dc/protected/views/animal/info.php
+++ server/dc/protected/views/animal/info.php
@@ -135,10 +135,10 @@ $(window).on('load',function(){
 			oLi.className='box';
 			oParent.appendChild(oLi);
 		var oA=document.createElement('a');
-			oA.setAttribute('href',"'"+<?php echo $this->createUrl('social/foodShareApi', array('img_id'=>$image['img_id']))?>+"'");
+			oA.setAttribute('href',"<?php echo $this->createUrl('social/foodShareApi', array('img_id'=>$image['img_id']))?>");
 			oLi.appendChild(oA);
 		var oImg=document.createElement('img');
-			oImg.src="'"+<?php echo "http://".OSS_PREFIX."4upload.oss-cn-beijing.aliyuncs.com/".$image['url']?>+"'";
+			oImg.src="<?php echo "http://".OSS_PREFIX."4upload.oss-cn-beijing.aliyuncs.com/".$image['url']?>";
 			oA.appendChild(oImg);
 	<?php }?>
 
