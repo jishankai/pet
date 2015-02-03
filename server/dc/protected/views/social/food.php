c1434ce8b46b2c194de405aaeb9ff616c4b37d9b
skji <jishankai@gmail.com>
2015-02-03 18:47:49 +0800

    UPDATE

--- server/dc/protected/views/social/food.php
+++ server/dc/protected/views/social/food.php
@@ -49,7 +49,7 @@ $signPackage = $jssdk->GetSignPackage();
       <div class="info_photo">
           <img src="http://<?php echo OSS_PREFIX?>4upload.oss-cn-beijing.aliyuncs.com/<?php echo $r['url']?>" alt="banner" class="active"/>
       </div>
-      <?php if($r['is_food']) {?>
+      <?php if($r['is_food']) {echo "<!--";?>
 			<div class="details clearfix">
 	        	<ul class="about_food">
 	            	<li  class="received"><div>已收到</div></li>
@@ -57,7 +57,7 @@ $signPackage = $jssdk->GetSignPackage();
 	                <li ><div class="time_img" id="time_img"></div></li>
 	            </ul>
         	</div>
-          <?php }?>
+          <?php echo "-->";}?>
         	<div class="black">
         	</div>
         	<div class="come_from">
@@ -111,7 +111,7 @@ $signPackage = $jssdk->GetSignPackage();
 		<div class="give_sc" id="give_sc">
             <h3>打赏成功～萌星<span><?php echo $r['name']?></span>感谢您的关爱</h3>
 		</div>
-    <?php if($r['is_food']) {?>
+    <?php if($r['is_food']) {echo "<!--";?>
 		<div class="give_btn">
 			<div class="give_btn_bottom">
 				<div class="give_btn_con">
@@ -125,7 +125,7 @@ $signPackage = $jssdk->GetSignPackage();
 				</div>
 			</div>
 		</div>
-    <?php }?>
+    <?php echo "-->";}?>
 		<!-- <div class="load">
 			
 			<div class="load_left">
