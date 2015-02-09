26bafc674941feb06c6aba1b70c9dd603338df1d
skji <jishankai@gmail.com>
2015-02-09 17:42:50 +0800

    UPDATE

--- server/dc/protected/views/social/ration.php
+++ server/dc/protected/views/social/ration.php
@@ -4,6 +4,7 @@
 		<meta name="viewport" content="width = device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<title>口粮页</title>
+        <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/ration.css">
 	</head>
 <body>
@@ -33,7 +34,6 @@
 	<div class="hr"></div>
 </div>
 </body>
-<script type="text/javascript" src="js/jquery-1.11.1.js"></script>
 <script type="text/javascript">
 
 $(window).on('load',function(){
