4fccd40386f6f0b005c2952cca0fcf72eeb39c93
skji <jishankai@gmail.com>
2015-02-09 19:26:35 +0800

    UPDATE

--- server/dc/protected/views/social/ration.php
+++ server/dc/protected/views/social/ration.php
@@ -47,7 +47,7 @@ $(window).on('load',function(){
          	$("#head1").attr('src','<?php echo "http://".OSS_PREFIX."4upload.oss-cn-beijing.aliyuncs.com/".$v['url']?>');
          	$("#food1").html('<?php echo $v['food']?>');
          	$("#con1").html('<?php echo $v['cmt']?>');
-         	$("#time1").html('<?php echo $v['create_time']?>');
+         	$("#time1").html(FreshTime(<?php echo $v['create_time']?>));
 
             $("#reward").click(function(){
               if (<?php echo (time()-$v['create_time'])>86400?0:1?>) {
@@ -74,12 +74,36 @@ $(window).on('load',function(){
            	var span2=$('<span>').html('<?php echo $v['food']?>').appendTo($(tit));
            	var span2=$('<span>').html(" 份").appendTo($(tit));
            	var con=$('<p>').addClass('con').html('<?php echo $v['cmt']?>').appendTo($(conBox));
-           	var time=$('<p>').addClass('time').html('<?php echo $v['create_time']?>').appendTo($(conBox));
+           	var time=$('<p>').addClass('time').html(FreshTime(<?php echo $v['create_time']?>)).appendTo($(conBox));
 
           	}
        <?php }?>
      /*})*/
  })
+function FreshTime(timestamp){
+            var endtime=new Date(timestamp);//结束时间
+            var nowtime = new Date();//当前时间
 
+            var lefttime= parseInt((endtime.getTime()-nowtime.getTime())/1000);
+            mm= parseInt(lefttime/3600/24/30);
+            d=  parseInt(lefttime/3600/24);
+            h=  parseInt(lefttime/(60*60)%24);
+            m=  parseInt(lefttime/(60)%60);
+            s=  parseInt(lefttime%60);
+
+
+            if(mm+1>1){
+                return mm+"个月前";
+            }
+            else if(d>0){
+                return d+"天前";
+            }
+            else if(h>0){
+                return h+"小时前";
+            }
+            else if(m>0){
+                return m+"分钟前";
+            }
+        }
 </script>
 </html>
