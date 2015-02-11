6fd7d8d4b1cf5ddb07e11647dc06206d549f65a9
skji <jishankai@gmail.com>
2015-02-11 14:21:29 +0800

    UPDATE

--- server/dc/protected/views/alipay/pay.php
+++ server/dc/protected/views/alipay/pay.php
@@ -14,13 +14,13 @@
 </style>
 </head>
 <script>
-    var _hmt = _hmt || [];
-    (function() {
-      var hm = document.createElement("script");
-      hm.src = "//hm.baidu.com/hm.js?fffd5628b5c5fe81d7a7867d554d07ca";
-      var s = document.getElementsByTagName("script")[0]; 
-      s.parentNode.insertBefore(hm, s);
-  })();
+  //   var _hmt = _hmt || [];
+  //   (function() {
+  //     var hm = document.createElement("script");
+  //     hm.src = "//hm.baidu.com/hm.js?fffd5628b5c5fe81d7a7867d554d07ca";
+  //     var s = document.getElementsByTagName("script")[0]; 
+  //     s.parentNode.insertBefore(hm, s);
+  // })();
 </script>
 <body>
 <div class="comWidth">
