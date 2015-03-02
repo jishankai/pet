0435a5bb92229b8f2ff97266c5e81a99d3117a6d
skji <jishankai@gmail.com>
2015-03-02 16:17:52 +0800

    UPDAET

--- server/dc/protected/commands/shell/NotifyEaserMobUsersCommand.php
+++ server/dc/protected/commands/shell/NotifyEaserMobUsersCommand.php
@@ -6,22 +6,15 @@ class NotifyEaserMobUsersCommand extends CConsoleCommand {
 	}
 	
     public function start() {
-        $easemob = Yii::app()->easemob;
-        $n = 20;
-        for ($i=0; $n >= 20; $i++) { 
-            $usr_ids = Yii::app()->db->createCommand('SELECT usr_id FROM dc_user ORDER BY usr_id DESC LIMIT :i,20')->bindValue(':i', 20*$i)->queryColumn();
-            $n = count($usr_ids);
-            $easemob->sendToUsers($usr_ids, NPC_SYSTEM_USRID, array(
-                'mixed'=>TRUE,
-                'msg'=>"hi地球银，\n
-新年过得好么？~新年活动已经进入最后冲刺阶段啦！\n第二周战况在此：http://t.cn/RwWYXF5 你有木有上榜呢？还没参加的小伙伴快戳：http://t.cn/Rw0KSlO 有独家定制惊喜大奖等你拿哦~~",
-                'ext'=>array(
-                    'nickname'=>'事务官',
-                ),
-            ));
-            sleep(5);
-        }
-
+        $easemob = Yii::app()->easemob;//初始化环信配置
+        $usr_ids = Yii::app()->db->createCommand('SELECT usr_id FROM dc_user ORDER BY usr_id DESC')->queryColumn();
+        $easemob->sendToUsers($usr_ids, NPC_SYSTEM_USRID, array(
+            'mixed'=>TRUE,
+            'msg'=>"hi地球银，\n新年过得好么？~新年活动已经进入最后冲刺阶段啦！\n第二周战况在此：http://t.cn/RwWYXF5 你有木有上榜呢？还没参加的小伙伴快戳：http://t.cn/Rw0KSlO 有独家定制惊喜大奖等你拿哦~~",
+            'ext'=>array(
+                'nickname'=>'事务官',
+            ),
+        ));
     }
 
     public function run($args) {
