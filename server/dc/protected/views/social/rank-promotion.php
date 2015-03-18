2d846396340277576159b6566f84f7abbf1867d9
hez <enzihuang@163.com>
2015-03-18 16:23:05 +0800

    rank-position PHP and CSS

---
+++ server/dc/protected/views/social/rank-promotion.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>官衔晋升说明页面</title>
+	<link rel="stylesheet" type="text/css" href="css/rank-promotion.css">
+</head>
+<body>
+<div class="wraper" id="timeline">
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<img src="css/images/rank-promotion/agent_btn.png"/>
+		</div>
+		<div class="right">
+			<h3>带自家宝贝<br/>入驻宠物星球</h3>
+		</div>
+	</div>
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<h3>捧<span>TA</span></h3>	
+		</div>
+		<div class="right">
+			<img src="css/images/rank-promotion/fans1_btn.png"/>
+		</div>
+	</div>
+
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<img src="css/images/rank-promotion/fans2_btn.png"/>
+		</div>
+		<div class="right">
+			<h3>贡献值达到<span>480</span></h3>	
+		</div>
+	</div>
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<h3>贡献值达到<span>660</span></h3>	
+		</div>
+		<div class="right">
+			<img src="css/images/rank-promotion/fans3_btn.png"/>
+		</div>
+	</div>
+
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<img src="css/images/rank-promotion/fans4_btn.png"/>
+		</div>
+		<div class="right">
+			<h3>贡献值达到<span>924</span></h3>	
+		</div>
+	</div>
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<h3>贡献值达到<span>1188</span></h3>	
+		</div>
+		<div class="right">
+			<img src="css/images/rank-promotion/fans5_btn.png"/>
+		</div>
+	</div>
+
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<img src="css/images/rank-promotion/fans6_btn.png"/>
+		</div>
+		<div class="right">
+			<h3>贡献值达到<span>1452</span></h3>	
+		</div>
+	</div>
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<h3>贡献值达到<span>1716</span></h3>	
+		</div>
+		<div class="right">
+			<img src="css/images/rank-promotion/fans7_btn.png"/>
+		</div>
+	</div>
+
+	<div class="timeline_block clearfix">
+		<div class="circle"></div>
+		<div class="left">
+			<img src="css/images/rank-promotion/fans8_btn.png"/>
+		</div>
+		<div class="right">
+			<h3>贡献值达到<span>1980</span></h3>	
+		</div>
+	</div>
+	<img src="css/images/rank-promotion/rank-promotion-word.png" class="rank_word"/>
+
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
