a227716ea6976bc5ade2da8445168931fd6bdc10
hez <enzihuang@163.com>
2015-03-19 16:11:23 +0800

    改占卜高度

--- server/dc/protected/views/social/divine.php
+++ server/dc/protected/views/social/divine.php
@@ -6,7 +6,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0,maximum-scale=1.0, user-scalable=no">
 <style type="text/css">
 *{margin: 0;padding: 0;}
-.wraper{width: 640px;height:9000px;margin: 0 auto;}
+.wraper{width: 640px;height:900px;margin: 0 auto;}
 img{width:100%;height: 100%;}
 #shake{display: none;}
 #sign{display: none;}
