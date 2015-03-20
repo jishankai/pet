d74f3af6794de5f725a36e3e7c37863b48c7160c
hez <enzihuang@163.com>
2015-03-20 16:32:05 +0800

    占卜页加微信分享标题及相应图片素材

--- server/dc/protected/views/social/divine.php
+++ server/dc/protected/views/social/divine.php
@@ -112,5 +112,39 @@ function jumpToDecode(){
     
 </script>
 
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+  // 注意：所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。 
+  // 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
+  // 完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
+  wx.config({
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+    jsApiList: [
+      // 所有要调用的 API 都要加到这个列表中
+      'checkJsApi',
+      'onMenuShareTimeline',
+      'onMenuShareAppMessage',
+      'onMenuShareQQ',
+      'onMenuShareWeibo',
+    ]
+  });
+  wx.ready(function () {
+    // 在这里调用 API
+    var shareData = {
+        title: "神秘占卜",
+        desc: "穿越到二次元，你会变成谁？",
+        link: "http://"+window.location.host+"/index.php?r=social/activityview&aid="+<?php echo $aid?>, 
+        imgUrl: "http://"+window.location.host+"/css/images/divine_tit.jpg"
+    };
+    wx.onMenuShareAppMessage(shareData);
+    wx.onMenuShareTimeline(shareData);
+    wx.onMenuShareQQ(shareData);
+    wx.onMenuShareWeibo(shareData);
+  });
+</script>
+
 
 </html>
