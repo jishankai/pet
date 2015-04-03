a52160264ced555d6d7708be7638f92677ddedca
hez <enzihuang@163.com>
2015-04-03 18:02:31 +0800

    修改充值活动页按钮位置问题

--- server/dc/protected/views/social/pay_active.php
+++ server/dc/protected/views/social/pay_active.php
@@ -5,25 +5,25 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title>充值活动</title>
 	<style>
+		
 		*{padding: 0;margin: 0;}
 		body{background: #efefef;}
 		.wrapper{width: 640px;margin: 0 auto;text-align: center;
 			}
-		.pay_active_btn{position: absolute;left: center;margin-left: -190px;
-			top:420px;}
+		.pay_active_btn{position: absolute;left: 50%;/*margin-left: -190px;*/
+			/*top:420px;*/}
 
 		@media only screen and (min-width: 300px) and (max-width: 640px){
 
 			.wrapper{width: 100%;}
 			.pay_active{width: 100%;height: auto;}
 			.pay_active_btn{width: 70%;height: auto;
-				margin-left:-35%;top:35%;}
+				/*margin-left:-35%;*//*top:35%;*/}
 		}
 
-		@media only screen and (min-height: 480px) and (max-height: 540px){
-			.pay_active_btn{width: 70%;height: auto;
-				margin-left:-35%;top:45%;}
-		}
+		/*@media only screen and (min-height: 480px) and (max-height: 540px){
+			.pay_active_btn{top:45%!important;}
+		}*/
 
 	</style>
 </head>
@@ -34,4 +34,14 @@
 </div>
 	
 </body>
+<script type="text/javascript" src="js/jquery-1.11.1.js"></script>
+<script type="text/javascript">
+$(function(){
+	var a=$(".pay_active_btn").width();
+	$(".pay_active_btn").css("margin-left",-a/2+"px");
+	var b=$(".pay_active").height();
+	$(".pay_active_btn").css("top",b*0.42+"px");
+})
+	
+</script>
 </html>
