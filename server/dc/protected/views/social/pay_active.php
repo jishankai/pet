a8e288d6284274110b72b527bd479343abb8e48b
hez <enzihuang@163.com>
2015-04-03 10:44:39 +0800

    充值活动页及相关图片

---
+++ server/dc/protected/views/social/pay_active.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width = device-width, initial-scale=1.0">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>充值活动</title>
+	<style>
+		*{padding: 0;margin: 0;}
+		.wrapper{width: 640px;margin: 0 auto;text-align: center;
+			background-color: #efefef;}
+		.pay_active_btn{margin-bottom: 50px;margin-top: 20px;}
+		@media only screen and (min-width: 300px) and (max-width: 640px){
+			.wrapper{width: 100%;}
+			.pay_active{width: 100%;height: auto;}
+			.pay_active_btn{width: 80%;height: auto;
+				margin-bottom: 30px;margin-top: 10px;}
+		}
+
+	</style>
+</head>
+<body>
+<div class="wrapper">
+	<img src="css/images/pay_active.jpg" class="pay_active"/>
+	<a href="#"><img src="css/images/pay_active_btn.jpg" class="pay_active_btn"/></a>
+</div>
+	
+</body>
+</html>
