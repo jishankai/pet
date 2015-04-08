ebcc86aa2bbe2c48b581275e96cf4c94b5f25cb5
skji <jishankai@gmail.com>
2015-04-08 18:23:17 +0800

    UPDATE

--- server/dc/protected/controllers/SocialController.php
+++ server/dc/protected/controllers/SocialController.php
@@ -133,9 +133,9 @@ class SocialController extends Controller
 
     public function actionArticles($page=0)
     {
-        $articles = Yii::app()->db->createCommand("SELECT * FROM dc_article WHERE image='' ORDER BY i.update_time DESC LIMIT :m, 10")->bindValue(':m', $page*10)->queryAll();
+        $articles = Yii::app()->db->createCommand("SELECT * FROM dc_article WHERE image='' ORDER BY update_time DESC LIMIT :m, 10")->bindValue(':m', $page*10)->queryAll();
         if ($page==0) {
-            $articles['banner'] = Yii::app()->db->createCommand("SELECT * FROM dc_article WHERE image!='' ORDER BY i.update_time DESC LIMIT 1")->queryRow();
+            $articles['banner'] = Yii::app()->db->createCommand("SELECT * FROM dc_article WHERE image!='' ORDER BY update_time DESC LIMIT 1")->queryRow();
         }
 
         $this->echoJsonData($articles);
