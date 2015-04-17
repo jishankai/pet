f1fde55de0d29f022ed7a0bf173542b10a591e37
skji <jishankai@gmail.com>
2015-04-17 14:08:58 +0800

    UPDATE

--- server/dc/protected/controllers/StarController.php
+++ server/dc/protected/controllers/StarController.php
@@ -14,7 +14,7 @@ class StarController extends Controller
     public function actionListApi()
     {
         $session = Yii::app()->session;
-        $stars = Yii::app()->db->createCommand('SELECT star_id, name, icon, title, description, banner, url, end_time FROM dc_star')->queryAll();
+        $stars = Yii::app()->db->createCommand('SELECT star_id, name, icon, title, description, banner, url, end_time FROM dc_star ORDER BY end_time DESC')->queryAll();
 
         foreach ($stars as $k => $v) {
             if (empty($session['usr_id'])) {
