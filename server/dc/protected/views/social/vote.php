be01f3f8a076fbefc1dd744d86fa8389f9f1fecc
skji <jishankai@gmail.com>
2015-04-29 16:40:58 +0800

    UPDATE

--- server/dc/protected/views/social/vote.php
+++ server/dc/protected/views/social/vote.php
@@ -1,3 +1,8 @@
+<?php
+require_once "jssdk.php";
+$jssdk = new JSSDK(WECHAT_MP_ID, WECHAT_MP_SECRET);
+$signPackage = $jssdk->GetSignPackage();
+?>
 <!DOCTYPE>
 <html>
 <head>
@@ -267,5 +272,39 @@
          }
 
 
+</script>
+<script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
+<script>
+  // 注意：所有的JS接口只能在公众号绑定的域名下调用，公众号开发者需要先登录微信公众平台进入“公众号设置”的“功能设置”里填写“JS接口安全域名”。 
+  // 如果发现在 Android 不能分享自定义内容，请到官网下载最新的包覆盖安装，Android 自定义分享接口需升级至 6.0.2.58 版本及以上。
+  // 完整 JS-SDK 文档地址：http://mp.weixin.qq.com/wiki/7/aaa137b55fb2e0456bf8dd9148dd613f.html
+  wx.config({
+    appId: '<?php echo $signPackage["appId"];?>',
+    timestamp: <?php echo $signPackage["timestamp"];?>,
+    nonceStr: '<?php echo $signPackage["nonceStr"];?>',
+    signature: '<?php echo $signPackage["signature"];?>',
+    jsApiList: [
+      // 所有要调用的 API 都要加到这个列表中
+      'checkJsApi',
+      'onMenuShareTimeline',
+      'onMenuShareAppMessage',
+      'onMenuShareQQ',
+      'onMenuShareWeibo',
+    ]
+  });
+  wx.ready(function () {
+    // 在这里调用 API
+    var shareData = {
+        title: "包吃包住包撒欢儿，宠物星球·我是大萌星携手BARKSTARS汪星客咖啡，第一任汪店长公开选拔开！始！啦！",
+        desc: "每天都有免费投票机会，快来给你喜欢的候选汪投票呀~",
+        link: "http://"+window.location.host+"/index.php?r=social/vote", 
+        imgUrl: "http://"+window.location.host+"/css/images/a2.jpg",
+        
+    };
+    wx.onMenuShareAppMessage(shareData);
+    wx.onMenuShareTimeline(shareData);
+    wx.onMenuShareQQ(shareData);
+    wx.onMenuShareWeibo(shareData);
+  });
 </script>
 </html>
