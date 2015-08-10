e0a3cbeb370d52b3f5e50c9923fe2b3dbc7eb9b5
季善凯 <jishankai@JishankaiOfficeMBP.local>
2015-08-10 14:43:00 +0800

    update

--- server/dc/protected/modules/admin/views/tool/index.php
+++ server/dc/protected/modules/admin/views/tool/index.php
@@ -8,6 +8,7 @@ $this->menu=array(
 	array('label'=>'切换用户', 'url'=>array('replace')),
 	array('label'=>'删除用户', 'url'=>array('clear')),
     array('label'=>'充值', 'url'=>array('gold')),
+    array('label'=>'设置密码', 'url'=>array('password')),
 );
 ?>
 
