bab36ef028089620143d77a586db6904c905458c
季善凯 <jishankai@qq.com>
2015-08-10 14:49:02 +0800

    update

--- server/dc/protected/modules/admin/controllers/ToolController.php
+++ server/dc/protected/modules/admin/controllers/ToolController.php
@@ -36,7 +36,7 @@ class ToolController extends Controller
 				'users'=>array('admin'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','replace','clear','gold'),
+                'actions'=>array('admin','replace','clear','gold','password'),
 				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
